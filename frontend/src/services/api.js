3aca0643ab55bdc7fe79f9836ef78bfcc89d832a
Joshie <joshuatevera@gmail.com>
2026-05-09 10:17:21 +0200

    Fix API base URL for production

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'https://programming-assignment-grading-and.onrender.com/admin'; // change for production
+const API_BASE_URL = const API_BASE_URL = 'https://programming-assignment-grading-and.onrender.com'; // change for production
 
 const api = axios.create({
   baseURL: API_BASE_URL,
